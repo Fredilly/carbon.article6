ad061c57774a72b8a0991b94d5c5222cbe07aac3
Fred E <fredilly@article6.org>
2026-08-24 12:24:23 +0800

    Add initial carbon page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Article6 Carbon</h1>
+      <p>Independent pre-validation evidence and methodology readiness review.</p>
+    </main>
+  );
+}
